fcb01adee359e8b258605831b37b62b4f3c59840
Spartan <bingwangzhao@163.com>
2016-08-08 09:30:28 +0800

    Object and Array

---
+++ Zhao.Exercise.WebUi/Areas/HTML/HTMLAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Zhao.Exercise.WebUi.Areas.HTML
+{
+    public class HTMLAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "HTML";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "HTML_default",
+                "HTML/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
