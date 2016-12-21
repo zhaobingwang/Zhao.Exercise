e474fed8d38e5fdf986205f0b8c01d814a90c04b
Administrator <Administrator@ZHAO-PC>
2016-12-21 12:41:07 +0800

    Linq

---
+++ Zhao.Exercise.WebUi/Areas/Linq/LinqAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Zhao.Exercise.WebUi.Areas.Linq
+{
+    public class LinqAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Linq";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Linq_default",
+                "Linq/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
