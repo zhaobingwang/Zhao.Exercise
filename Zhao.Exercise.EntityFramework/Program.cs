6bb627f5844a3aec9c2072c77563b4f13ef799f8
Spartan <zhaobingwang@outlook.com>
2017-01-23 15:45:42 +0800

    EF

---
+++ Zhao.Exercise.EntityFramework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhao.Exercise.EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
