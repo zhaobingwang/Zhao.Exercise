ec67284005320c39ab7b3b61671a239d4a8c7329
百山尧 <zhaobingwang@outlook.com>
2017-05-24 23:15:58 +0800

    lamnda1

--- Zhao.Exercise.Lambda/Program.cs
+++ Zhao.Exercise.Lambda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,24 @@ namespace Zhao.Exercise.Lambda
 {
     class Program
     {
+        delegate int del(int i);
+        delegate bool compare(string str, int length);
         static void Main(string[] args)
         {
+            //del myDelegate = x => x * x;
+            //Console.WriteLine(myDelegate(5));
+            //Expression<del> myET = x => x * x;
+            //Console.WriteLine(CompareStrLength("aaa",3));
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static bool CompareStrLength(string str, int length)
+        {
+            compare compare = (string s, int len) => str.Length > len;
+            return compare(str, length);
         }
     }
 }
