83e0e7511bd0b64191568ef55329a1255ae71c6c
百山尧 <zhaobingwang@outlook.com>
2017-05-31 13:00:30 +0800

    lambda

---
+++ Zhao.Exercise.WebUi/Areas/Lambda/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace Zhao.Exercise.WebUi.Areas.Lambda.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Lambda/Default
+        public  ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
