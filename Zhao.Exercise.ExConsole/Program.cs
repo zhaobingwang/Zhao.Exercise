902d6c0cb5d7f75c0f20975521e950df6326f8d1
百山尧 <zhaobingwang@outlook.com>
2017-06-05 20:25:54 +0800

    对象浅拷贝，深拷贝

--- Zhao.Exercise.ExConsole/Program.cs
+++ Zhao.Exercise.ExConsole/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,18 +28,31 @@ namespace Zhao.Exercise.ExConsole
             //Console.WriteLine("-------------------------------------------------------"); 
             #endregion
 
-            string tempBankNo = "123456789087654598";
-            for (int iBankNo = 4; iBankNo < tempBankNo.Length; iBankNo += 4 + 1)
-            {
-                tempBankNo += tempBankNo.Insert(iBankNo, "-");//灾难代码
-            }
-            Console.WriteLine(tempBankNo);
-            Console.ReadKey();
+            //string tempBankNo = "123456789087654598";
+            //for (int iBankNo = 4; iBankNo < tempBankNo.Length; iBankNo += 4 + 1)
+            //{
+            //    tempBankNo += tempBankNo.Insert(iBankNo, "-");//灾难代码
+            //}
+            //Console.WriteLine(tempBankNo);
+            //Console.ReadKey();
+
+
+            TestClass class1 = new TestClass();
+            TestClass class2 = new TestClass();
+            class1.str1 = "abc";
+            class1.int1 = 1;
+            class2 = class1;//(TestClass)Clone2.CloneObject(class1);//(TestClass)class1.Clone();
+            class2.str1 = "123";
+            class2.int1 = 2;
+            Console.WriteLine(Object.ReferenceEquals(class1,class2));
+            Console.WriteLine(class1.str1+class2.str1);
+            Console.WriteLine(class1.int1+class2.int1);
+
         }
     }
     class ZhaoEx
     {
-        public List<int> ChangeTwoVariable(int i,int j)
+        public List<int> ChangeTwoVariable(int i, int j)
         {
             i = i + j;//此时i是i+j
             j = i - j;//此时j是初始的i的值
@@ -45,6 +61,41 @@ namespace Zhao.Exercise.ExConsole
             result.Add(i);
             result.Add(j);
             return result;
+        }
+    }
+    [Serializable]
+    class TestClass:ICloneable
+    {
+        public string str1 { get; set; }
+        public string str2 { get; set; }
+        public int int1 { get; set; }
+
+        //object ICloneable.Clone()
+        //{
+        //    return this.Clone();
+        //}
+        //public TestClass Clone()
+        //{
+        //    return (TestClass)this.MemberwiseClone();
+        //}
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+    class Clone2
+    {
+        //此方法必须把诶标记为 Serializable
+        public static object CloneObject(object obj)
+        {
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                BinaryFormatter binaryFormatter = new BinaryFormatter(null,
+                     new StreamingContext(StreamingContextStates.Clone));
+                binaryFormatter.Serialize(memStream, obj);
+                memStream.Seek(0, SeekOrigin.Begin);
+                return binaryFormatter.Deserialize(memStream);
+            }
         }
     }
 }
